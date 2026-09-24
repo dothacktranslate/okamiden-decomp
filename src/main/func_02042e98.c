f14966a2356f3dccc331e8a627a213403cf47ea6
kazetrigger <kazetrigger@gmail.com>
2026-09-24 00:19:27 -0230

    okamiden: add matched mode wrapper functions

---
+++ src/main/func_02042e98.c
@@ -0,0 +1,21 @@
+#pragma thumb on
+
+#include "src/main/func_0203a.h"
+
+extern void func_02042c8c(
+    void *context,
+    void *descriptor,
+    s32 mode
+);
+
+void func_02042e98(
+    void *context,
+    void *descriptor
+)
+{
+    func_02042c8c(
+        context,
+        descriptor,
+        3
+    );
+}
